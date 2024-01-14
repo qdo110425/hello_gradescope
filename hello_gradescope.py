d45bdee0ad033303a53bf83084d4886f8e40b2c3
project2a <qdo110425@gmail.com>
2024-01-14 13:22:02 -0600

    hello_gradescope.py

--- hello_gradescope.py
+++ hello_gradescope.py
@@ -2,7 +2,7 @@
 # OSU email: doq@oregonstate.edu
 # Course: CS261 - Data Structures
 # Assignment: hello_gradescope
-# Due Date:
+# Due Date: 01/17/2024
 # Description: submitting the hello_gradescope.py file to Gradescope to confirm that you are correctly connected.
 
 def gradescope_test():
