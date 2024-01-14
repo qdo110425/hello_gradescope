04ecf718fe2f665644d07bace853fb58930d57d6
project2a <qdo110425@gmail.com>
2024-01-14 13:08:49 -0600

    Initial commit

---
+++ hello_gradescope.py
@@ -0,0 +1,9 @@
+# Name: Quyen Do
+# OSU email: doq@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: hello_gradescope
+# Due Date:
+# Description: submitting the hello_gradescope.py file to Gradescope to confirm that you are correctly connected.
+
+def gradescope_test():
+    return 42
